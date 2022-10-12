118a61713dea6e9205780d749cde4fe2d71f2c7b
DSP-Tan <danielsptanner@gmail.com>
2022-10-12 22:12:29 +0200

    commit before account expires

--- Large-LaMoX/Model_1L/Run.sh
+++ Large-LaMoX/Model_1L/Run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Submission script for nic5
 #SBATCH --job-name=LaM_1L
-#SBATCH --time=9:00:00
+#SBATCH --time=12:00:00
 #SBATCH --ntasks=60
 #SBATCH --mem-per-cpu=1548
 #SBATCH --partition=batch
